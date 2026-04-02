dd99289cabce955784a62954e44026a3f929f5b8
asha1817 <ashasep1817@gmail.com>
2026-04-02 12:26:02 +0530

    Enhance update function and add market tips

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 let balance = 10000;
+let streak = localStorage.getItem("streak") || 1;
 
-function update() {
-  document.getElementById("balance").innerText = "Balance: ₹" + balance;
+const tips = [
+  "Always use stop loss",
+  "Don't invest blindly",
+  "Learn before trading",
+  "Control emotions"
+];
+
+const markets = [
+  "Market is bullish today",
+  "Market is slightly down",
+  "Market is sideways"
+];
+
+document.getElementById("market").innerText =
+  markets[new Date().getDay() % markets.length];
+
+document.getElementById("tip").innerText =
+  tips[new Date().getDay() % tips.length];
+
+document.getElementById("streak").innerText =
+  "Day " + streak;
+
+function showPage(page) {
+  document.querySelectorAll(".page").forEach(p => p.style.display = "none");
+  document.getElementById(page).style.display = "block";
 }
+
+showPage("home");
 
 function buy() {
   balance -= 100;
@@ -14,6 +40,14 @@ function sell() {
   update();
 }
 
-function learn() {
-  alert("Start learning basics of stock market!");
+function update() {
+  document.getElementById("balance").innerText = "₹" + balance;
+}
+
+function check(ans) {
+  if (ans === 1) {
+    document.getElementById("result").innerText = "Correct!";
+  } else {
+    document.getElementById("result").innerText = "Wrong!";
+  }
 }
